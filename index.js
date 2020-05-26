65c7aed0918c69f690dc4270451197e677d68768
DavideViolante <fungtq3@gmail.com>
2020-05-26 12:03:53 +0200

    Create function to call investing

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const axios = require('axios')
 const { mapResponse } = require('./functions')
 
+function callInvesting (pairId) {
+  return axios({
+    method: 'GET',
+    url: 'https://www.investing.com/common/modules/js_instrument_chart/api/data.php',
+    params: {
+      pair_id: pairId,
+      pair_interval: '86400',
+      chart_type: 'area', // 'area', 'candlestick'
+      candle_count: '120',
+      volume_series: 'yes',
+      events: 'yes',
+      period: ''
+    },
+    headers: {
+      'Referer': 'https://www.investing.com/',
+      'X-Requested-With': 'XMLHttpRequest'
+    }
+  })
+}
+
 async function investing (pairId) {
   try {
     if (!pairId) {
       throw Error('No pairId input provided')
     }
-    const response = await axios({
-      method: 'GET',
-      url: 'https://www.investing.com/common/modules/js_instrument_chart/api/data.php',
-      params: {
-        pair_id: pairId,
-        pair_interval: '86400',
-        chart_type: 'area', // 'area', 'candlestick'
-        candle_count: '120',
-        volume_series: 'yes',
-        events: 'yes',
-        period: ''
-      },
-      headers: {
-        'Referer': 'https://www.investing.com/',
-        'X-Requested-With': 'XMLHttpRequest'
-      }
-    })
+    const response = await callInvesting(pairId)
     if (!response.data.candles) {
       throw Error('No response')
     }
