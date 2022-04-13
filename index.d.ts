7cd2580ce0175240813edb7ea86a5be2815589ee
DavideViolante <fungtq3@gmail.com>
2022-04-13 10:36:59 +0200

    Fix and improve typescript types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
 declare module "investing-com-api" {
-    export function investing(input: string, period?: string, interval?: string, pointscount?: number): any[];
+  export function investing(
+    input: string,
+    period?: 'P1D' | 'P1W' | 'P1M' | 'P3M' | 'P6M' | 'P1Y' | 'P5Y' | 'MAX',
+    interval?: 'PT1M' | 'PT5M' | 'PT15M' | 'PT30M' | 'PT1H' | 'PT5H' | 'P1D' | 'P1W' | 'P1M',
+    pointscount?: 60 | 70 | 120
+  ): { date: number, value: number }[];
 }
